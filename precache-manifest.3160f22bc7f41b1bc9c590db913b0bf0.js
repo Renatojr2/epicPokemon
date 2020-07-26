e0695e1045a1130fdfb5ca64f8ba5caaf0d80c0a
Renato Junior <momentum323@gmail.com>
2020-07-26 12:31:00 -0300

    Updates

---
+++ precache-manifest.3160f22bc7f41b1bc9c590db913b0bf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d06cf6c86c9313e7eb67409f1eb06147",
+    "url": "/epicPokemon/index.html"
+  },
+  {
+    "revision": "d7e7736e594b085e9d1b",
+    "url": "/epicPokemon/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "d7e7736e594b085e9d1b",
+    "url": "/epicPokemon/static/js/2.a34e69df.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/epicPokemon/static/js/2.a34e69df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4dbfb8d0544807cd94e",
+    "url": "/epicPokemon/static/js/main.45b4d039.chunk.js"
+  },
+  {
+    "revision": "d18664c750b5c1bf303a",
+    "url": "/epicPokemon/static/js/runtime-main.345fe67d.js"
+  }
+]);
